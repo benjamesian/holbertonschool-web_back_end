225c0d0ffbc996599cca30b9b944a9c58c345c4f
Ben Cook <808@holbertonschool.com>
2020-07-21 18:17:48 -0400

    returned list is sorted

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Basic async function"""
-import asyncio
+from asyncio import create_task, as_completed
 from typing import List
 
 WAIT_RANDOM = __import__('0-basic_async_syntax').wait_random
@@ -8,4 +8,5 @@ WAIT_RANDOM = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Run wait_random n times in parallel"""
-    return await asyncio.gather(*(WAIT_RANDOM(max_delay) for _ in range(n)))
+    tasks = [create_task(WAIT_RANDOM(max_delay)) for _ in range(n)]
+    return [await task for task in as_completed(tasks)]
