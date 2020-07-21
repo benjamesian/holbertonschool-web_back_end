74c489dc4e6e25847586cf5a5b7f39416bfc5df5
Ben Cook <808@holbertonschool.com>
2020-07-21 18:28:15 -0400

    return sorted output

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Basic async function"""
-import asyncio
+from asyncio import as_completed
 from typing import List
 
 TWR = __import__('3-tasks').task_wait_random
@@ -8,4 +8,5 @@ TWR = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Run wait_random n times in parallel"""
-    return await asyncio.gather(*(TWR(max_delay) for _ in range(n)))
+    tasks = [TWR(max_delay) for _ in range(n)]
+    return [await task for task in as_completed(tasks)]
