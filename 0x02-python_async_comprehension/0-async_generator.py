ee71463ae3a7a33c8795de5afaf568ead7dc1b0f
Ben Cook <808@holbertonschool.com>
2020-07-22 16:29:46 -0400

    fix return type

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ from random import uniform
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Generator that yields 10 random floats between 0 and 10"""
     for _ in range(10):
         await asyncio.sleep(1)
