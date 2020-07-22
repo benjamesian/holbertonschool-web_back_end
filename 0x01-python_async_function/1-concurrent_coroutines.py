f55710752b284fbbe6bc108704432a2355b4e9fc
Ben Cook <808@holbertonschool.com>
2020-07-22 04:26:13 -0400

    don't wrap coros in Tasks

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Basic async function"""
-from asyncio import create_task, as_completed
+from asyncio import as_completed
 from typing import List
 
 WAIT_RANDOM = __import__('0-basic_async_syntax').wait_random
@@ -8,5 +8,5 @@ WAIT_RANDOM = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Run wait_random n times in parallel"""
-    tasks = [create_task(WAIT_RANDOM(max_delay)) for _ in range(n)]
-    return [await task for task in as_completed(tasks)]
+    coros = [WAIT_RANDOM(max_delay) for _ in range(n)]
+    return [await task for task in as_completed(coros)]
