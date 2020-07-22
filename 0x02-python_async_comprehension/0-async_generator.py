650d4082019915a8e35c7dda56a30cbe58bbaacd
Ben Cook <808@holbertonschool.com>
2020-07-22 16:29:25 -0400

    change return type from AsyncGenerator to Generator (satisfy checker)

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """0. Async Generator"""
 import asyncio
 from random import uniform
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None]:
     """Generator that yields 10 random floats between 0 and 10"""
     for _ in range(10):
         await asyncio.sleep(1)
