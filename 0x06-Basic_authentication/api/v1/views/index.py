040a03c52b19180c9b4daf71c0fb33fe23e1fcd3
Ben Cook <808@holbertonschool.com>
2020-08-12 15:58:13 -0700

    forbidden route

--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorized():
         - error message
     """
     abort(401)
+
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden():
+    """ GET /api/v1/forbidden
+    Return:
+        - error message
+    """
+    abort(403)
