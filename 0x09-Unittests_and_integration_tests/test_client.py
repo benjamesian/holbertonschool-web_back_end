d48a443875f9684932b0b9adb8bc40db4d3e8117
Ben Cook <808@holbertonschool.com>
2020-08-25 23:23:44 -0700

    task 7

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """test GithubOrgClient"""
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -21,3 +21,12 @@ class TestGithubOrgClient(unittest.TestCase):
         resp = client.org
         self.assertEqual(payload, resp)
         mock_get_json.assert_called_once()
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)])
+    def test_has_license(self, repo, lisc_key, out):
+        """test_has_liscence"""
+        client = GithubOrgClient("google")
+        res = client.has_license(repo, lisc_key)
+        self.assertEqual(res, out)
