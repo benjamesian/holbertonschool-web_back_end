48ac6f77bd58e704e722a8f6408c3151ca35143e
Ben Cook <808@holbertonschool.com>
2020-09-16 12:19:44 -0700

    use a template string

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()]).then(data => {
     const [photoData, userData] = data;
-    console.log(photoData.body, userData.firstName, userData.lastName);
+    console.log(`${photoData.body} ${userData.firstName} ${userData.lastName}`);
   }, _ => {
     console.log('Signup system offline');
   });
