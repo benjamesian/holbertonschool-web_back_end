ed76ea73197bebd86387f6ae8d0fb915ae99e280
Ben Cook <808@holbertonschool.com>
2020-09-16 12:10:50 -0700

    fix typo in response body

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 export default function handleResponseFromAPI(promise) {
   return promise.then(_ => {
     console.log('Got a response from the API');
-    return { status: 200, body: 'Success' };
+    return { status: 200, body: 'success' };
   }, _ => {
     console.log('Got a response from the API');
     return Error();
