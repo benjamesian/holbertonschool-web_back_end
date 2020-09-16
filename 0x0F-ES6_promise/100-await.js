fb88479b40acfbc7877203e90cae82f06fc73575
Ben Cook <808@holbertonschool.com>
2020-09-16 13:12:27 -0700

    eslint style

--- 0x0F-ES6_promise/100-await.js
+++ 0x0F-ES6_promise/100-await.js
@@ -1,11 +1,11 @@
-import { uploadPhoto, createUser } from './utils'
+import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   const output = {
     photo: null,
     user: null,
-  }
-  return Promise.all([uploadPhoto(), createUser()]).then(data => {
+  };
+  return Promise.all([uploadPhoto(), createUser()]).then((data) => {
     const [photoData, userData] = data;
     output.photo = photoData;
     output.user = userData;
