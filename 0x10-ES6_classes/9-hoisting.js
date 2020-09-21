23b7b767c0564f21ad51448fba61e861fc4f8c36
Ben Cook <808@holbertonschool.com>
2020-09-20 22:49:08 -0700

    use var and disable eslint for var

--- 0x10-ES6_classes/9-hoisting.js
+++ 0x10-ES6_classes/9-hoisting.js
@@ -1,3 +1,19 @@
+// eslint-disable-next-line
+var class2019 = new HolbertonClass(2019, 'San Francisco');
+// eslint-disable-next-line
+var class2020 = new HolbertonClass(2020, 'San Francisco');
+
+// eslint-disable-next-line
+var student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
+// eslint-disable-next-line
+var student2 = new StudentHolberton('John', 'Doe', class2020);
+// eslint-disable-next-line
+var student3 = new StudentHolberton('Albert', 'Clinton', class2019);
+// eslint-disable-next-line
+var student4 = new StudentHolberton('Donald', 'Bush', class2019);
+// eslint-disable-next-line
+var student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+
 export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
@@ -12,9 +28,6 @@ export class HolbertonClass {
     return this._location;
   }
 }
-
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
 
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
@@ -31,11 +44,5 @@ export class StudentHolberton {
     return this._holbertonClass;
   }
 }
-
-const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
-const student2 = new StudentHolberton('John', 'Doe', class2020);
-const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
-const student4 = new StudentHolberton('Donald', 'Bush', class2019);
-const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
 export const listOfStudents = [student1, student2, student3, student4, student5];
