307db73ce2c45cce8ec9202cd9250d4643a8603c
Ben Cook <808@holbertonschool.com>
2020-10-01 22:08:37 -0700

    task 1

---
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+const { read } = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('Welcome to Holberton School, what is your name?\n', (ans) => {
+    console.log(`Your name is: ${ans}`);
+    console.log('This important software is now closing');
+    process.exit();
+});
