7d39bb9cfb346f6849cf97a0956b402c7ffceaeb
Ben Cook <808@holbertonschool.com>
2020-10-16 11:17:54 -0700

    remove main

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -81,26 +81,3 @@ class Cache:
     def get_int(self, key: str) -> int:
         """get an int"""
         return self.get(key, int)
-
-
-if __name__ == '__main__':
-    print(1)
-    cache = Cache()
-
-    TEST_CASES = {
-        b"foo": None,
-        123: int,
-        "bar": lambda d: d.decode("utf-8")
-    }
-
-    for value, fn in TEST_CASES.items():
-        key = cache.store(value)
-        assert cache.get(key, fn=fn) == value
-
-    print(11)
-    key = cache.store(b'foo')
-    print(cache.get(key))
-    # print(cache.get(key, int))
-
-    print(4)
-    replay(cache.store)
