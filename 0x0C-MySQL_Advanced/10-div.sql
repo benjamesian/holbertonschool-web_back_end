8d802c255e4084830b234a99fef7046e0f102153
Ben Cook <808@holbertonschool.com>
2020-10-29 23:42:56 -0700

    add tsak 10

---
+++ 0x0C-MySQL_Advanced/10-div.sql
@@ -0,0 +1,16 @@
+-- safe dividewoooo
+DELIMITER //
+
+DROP FUNCTION IF EXISTS SafeDiv;
+
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT
+BEGIN
+    DECLARE ret FLOAT;
+    IF b = 0 THEN
+        SET ret = 0;
+    ELSE
+        SET ret = a / b;
+    END IF;
+    RETURN ret;
+END //
